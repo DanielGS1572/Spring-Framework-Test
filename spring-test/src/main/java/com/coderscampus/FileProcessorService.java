3b231753a02e65a4953e1aad57a4efb334a77b58
DanielGS1572 <gonzalisd@gmail.com>
2017-01-22 22:47:00 -0600

    Observaciones del primer video [Terminado]

--- spring-test/src/main/java/com/coderscampus/FileProcessorService.java
+++ spring-test/src/main/java/com/coderscampus/FileProcessorService.java
@@ -3,20 +3,39 @@ package com.coderscampus;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
-//test
-@Component
-public class FileProcessorService
-{
-  @Autowired
-  FileReadingService frs;
-  
-  public void processFile ()
-  {
-    List<String> lines = frs.readFile("crime_report.csv");
-    for (String line : lines)
-    {
-      System.out.println(line);
-    }
-  }
+//git show-branch --al
+//git checkout origin/Part-1
+
+//git status
+//git add .
+//git commit -m "mensaje de branch"
+//git push origin HEAD:Part-1
+
+//Este archivo se encargará unicamente de procesar
+
+//Service
+//Component
+//Controller
+//... todos extienden de component y son lo mismo
+@Component(value="customName")
+public class FileProcessorService {
+	// El autowired lo que hace es un new
+	// se llama inyección de dependencia porque este objeto
+	// (FileProcessorService) depende de otro (FileReadingService)
+
+	// inversion of control es el mecanismo que spring tiene para hacer la
+	// inyeccion de dependencias (a traves del container). Es el control que
+	// spring tiene sobre los objetos y administrarlos
+	
+	@Autowired //dependency managed by spring
+	FileReadingService frs;
+
+	public void processFile() {
+		List<String> lines = frs.readFile("crime_report.csv");
+		for (String line : lines) {
+			System.out.println(line);
+		}
+	}
 }
--- spring-test/src/main/java/com/coderscampus/FileReadingApp.java
+++ spring-test/src/main/java/com/coderscampus/FileReadingApp.java
@@ -5,20 +5,32 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-public class FileReadingApp
-{
-  public static void main (String[] args)
-  {
-    ApplicationContext context = new AnnotationConfigApplicationContext(FileReadingApp.class);
-    
-    // the application context contains a component model
-    
-    // beans -> Component Model -> Application Context -> Spring Framework
-    
-    FileProcessorService fps = (FileProcessorService) context.getBean("fileProcessorService");
-    
-    fps.processFile();
-  }
+@Configuration  
+@ComponentScan //Se encarga de crear el component model
+public class FileReadingApp {
+	// Como se trata de una clase java lleva public static void main
+	// si fuera una clase de java web app no llevaría public static void main
+
+	public static void main(String[] args) {
+		// El core de spring tiene un application context
+		// Actualmente se usa a traves de anotaciones y ya no se interactua con
+		// el application context
+		// Antes si se interactuaba directamente y se hacia a traves de un
+		// archivo de configuracion xml
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(FileReadingApp.class);
+
+		// the application context contains a component model
+		// el component model es como el DOM y contiene todos los objeto que
+		// hacen la aplicacion y como los objetos de java interactuan
+
+		// beans -> Component Model -> Application Context -> Spring Framework
+		// getBean hace que Spring este consciente de este objeto
+		// sprign context se encarga de la instanciacion del objeto (usando las anotaciones)
+			//@Component a nivel clase --> esto hace que este en el component model
+		FileProcessorService fps = (FileProcessorService) context.getBean("customName");
+
+//		Los beans creados son de tipo singleton, solo hay uno solo
+		fps.processFile();
+	}
 }
