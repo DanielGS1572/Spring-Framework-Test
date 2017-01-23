903708f34cee7e378416449a1a49708e56c6d4cd
DanielGS1572 <gonzalisd@gmail.com>
2017-01-22 21:13:43 -0600

    prueba add

--- spring-test/src/main/java/com/coderscampus/FileProcessorService.java
+++ spring-test/src/main/java/com/coderscampus/FileProcessorService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+//test
 @Component
 public class FileProcessorService
 {
