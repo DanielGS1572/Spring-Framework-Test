8ba924629c197912c5d35cc21b53ca74c08e6521
DanielGS1572 <Admin@imac-de-angel.local>
2017-01-27 20:33:51 -0600

    ok

--- spring-test/src/main/java/com/coderscampus/FileReadingConfiguration.java
+++ spring-test/src/main/java/com/coderscampus/FileReadingConfiguration.java
@@ -16,6 +16,13 @@ import org.springframework.core.env.Environment;
 @EnableAutoConfiguration
 public class FileReadingConfiguration
 {
+	
+	//El modelo vista controlador va muy de la mano con web applications
+	// Usuario (usa) --> controlador (manipula) --> Modelo (Actualiza) --> vista (Vee) --> Usuario
+	// el usuario envia una peticion a traves de la URL (a traves de esta petición se ve si esta mapeada a un controlador)
+	// el controller (@Controller a nivel clase) intercepta web requests y se mapea con algun metodo a traves de @RequestMapping("/login") y regularmente esta en un metodo. Puede estar a nivel clase 
+	// el modelo usa el ModelMap es como un hashmap o un diccionario de datos. Ahí guarda todos los objetos. El modelo puede ir vacio
+	// la vista presenta lo que contiene el ModelMap y es un HTML o un JSP. Thymeleaf es un framework que puede inyectar objetos en HTML
   @Bean
   @Autowired
   static PropertySourcesPlaceholderConfigurer propertySourcesPlacholdConfigurer(Environment env)
@@ -25,6 +32,7 @@ public class FileReadingConfiguration
 
   public static void main(String[] args)
   {
+	  //Regularmente si se esta construyendo una aplicación web no se interactua directamente de inmediato con el contexto
     SpringApplication.run(FileReadingConfiguration.class, args);
   }
 }
--- spring-test/src/main/java/com/coderscampus/web/CrimeReportController.java
+++ spring-test/src/main/java/com/coderscampus/web/CrimeReportController.java
@@ -7,26 +7,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.coderscampus.CrimeReport;
 
+//Al controller siempre se le pondra en el nombre la palabra controller para poder identificarlo e ir dentro en un paquete especifico
 @Controller
-public class CrimeReportController
-{
-  // methods inside a controller will be "invoked" when a user
-  //  send a request to a particular URL
-  
-  @Autowired
-  private CrimeReport crimeReport;
-  
-  @RequestMapping("/crimeReport")
-  public String showReport (ModelMap model)
-  {
-    crimeReport.generateReport();
-    model.put("rows", crimeReport.getRows());
-    
-    // prepended text "src/main/resources/templates/"
-    // actual text "report"
-    // appended text ".html"
-    // final path: src/main/resources/templates/report.html
-    
-    return "report";
-  }
+public class CrimeReportController {
+	// methods inside a controller will be "invoked" when a user
+	// send a request to a particular URL
+
+	@Autowired
+	private CrimeReport crimeReport;	// Se instancia el reporteador
+
+	@RequestMapping("/crimeReport") // cuando introduzcan una URL la
+									// interceptara y hara match y entrara a
+									// este metodo
+	public String showReport(ModelMap model) // Cuando se usa ModelMap no se
+												// tiene que hacer autowired, de
+												// esto se encarga spring
+												// (siempre y cuando se le pase
+												// como parametro al metodo y la
+												// clase tenga la anotacion de
+												// controller)
+	{
+		crimeReport.generateReport();
+		model.put("rows", crimeReport.getRows());		//Aqui se mete la información al modelo
+
+		// prepended text "src/main/resources/templates/"
+		// actual text "report"
+		// appended text ".html"
+		// final path: src/main/resources/templates/report.html
+
+		return "report"; // este return regresa la vista que es un HTML o un JSP
+							// --> que esta dentro de
+							// resources/templates/report.html (spring boot esta
+							// configurado que busque por default en esta
+							// ubicacion)
+		// en el html se va a imprimir el resultado del reporte
+	}
 }
